159b77b64e16fa0b107321fb73ec79faf3686fe0
VincentZhang <vincentzhang@outlook.es>
2021-06-19 15:38:41 +0200

    Add test class

--- src/test/java/com/example/springbootconditiondemo/SpringbootConditionDemoApplicationTests.java
+++ src/test/java/com/example/springbootconditiondemo/SpringbootConditionDemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.springbootconditiondemo;
 
+import com.example.springbootconditiondemo.config.ApplicationConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 @SpringBootTest
 class SpringbootConditionDemoApplicationTests {
 
   @Test
-  void contextLoads() {
+  public void testPropertyWhenIsValidRange() {
+    ApplicationContextRunner context = new ApplicationContextRunner()
+        .withPropertyValues("car.age.min=5")
+        .withPropertyValues("car.age.max=50")
+        .withUserConfiguration(ApplicationConfig.class);
+
+    context.run(it -> Assertions.assertTrue(it.containsBean("car")));
+  }
+
+  @Test
+  public void testPropertyWhenIsNotValidRange() {
+    ApplicationContextRunner context = new ApplicationContextRunner()
+        .withPropertyValues("car.age.min=-5")
+        .withPropertyValues("car.age.max=5000000")
+        .withUserConfiguration(ApplicationConfig.class);
+
+    context.run(it -> Assertions.assertFalse(it.containsBean("car")));
   }
 
 }
